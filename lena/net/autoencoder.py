b6248e2bb5c81de2f275b5fb187ccb6db5525b14
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2021-07-01 12:42:41 +0200

    updated autoencoder class comments

--- lena/net/autoencoder.py
+++ lena/net/autoencoder.py
@@ -6,10 +6,40 @@ import numpy as np
 
 
 class Autoencoder(nn.Module):
+    """
+    Class for learning a nonlinear transformation function T_star.
+    Use either loss_auto for training autonomous experiments or
+    loss_noise for learning eigenvalues.
+    """
+
     def __init__(self, observer: LuenebergerObserver, params, device):
+        """Constructs autoencoder instance.
+
+        Arguments:
+            observer: An instance of the observer we want to train our
+                transformation function for.
+            parms: A list of model options like epoch, or batch size to configure
+                the autoencoder.
+
+        List of params:
+            batch_size: int
+            epochs: int
+            num_hlayers: int
+            size_hlayers: int
+            activation: string (tanh, relu)
+            lr: float
+            lr_milestones: array[float]
+            lr_gamma: float
+            recon_lambda: float
+            is_tensorboar: bool
+            shuffle: bool
+
+        Example:
+            model = Autoencoder(observer, params)
+            z, x_hat = model(data)
+            loss, loss_1, loss_2 = loss_auto(x, x_hat, z_hat)
         """
-        Autoencoder(observer, params) - class for learning nonlinear transformations
-        """
+        # Init super class
         super(Autoencoder, self).__init__()
 
         # Namespace observer, params and device
@@ -17,9 +47,11 @@ class Autoencoder(nn.Module):
         self.params = params
         self.device = device
 
+        # Set x and z dimension
         dim_x = observer.dim_x + observer.optionalDim
         dim_z = observer.dim_z + observer.optionalDim
 
+        # Set model params
         numHL = params['num_hlayers']
         sizeHL = params['size_hlayers']
 
@@ -29,7 +61,7 @@ class Autoencoder(nn.Module):
         elif params['activation'] == "tanh":
             self.act = nn.Tanh()
 
-        # Encoder architecture
+        # Define encoder architecture
         self.encoderLayers = nn.ModuleList()
         self.encoderLayers.append(nn.Linear(dim_x, sizeHL))
         for i in range(numHL):
@@ -37,7 +69,7 @@ class Autoencoder(nn.Module):
             self.encoderLayers.append(nn.Linear(sizeHL, sizeHL))
         self.encoderLayers.append(nn.Linear(sizeHL, dim_z))
 
-        # Decoder architecture
+        # Define decoder architecture
         self.decoderLayers = nn.ModuleList()
         self.decoderLayers.append(nn.Linear(dim_z, sizeHL))
         for i in range(numHL):
@@ -45,52 +77,89 @@ class Autoencoder(nn.Module):
             self.decoderLayers.append(nn.Linear(sizeHL, sizeHL))
         self.decoderLayers.append(nn.Linear(sizeHL, dim_x))
 
-    def encoder(self, x):
+    def encoder(self, x: torch.tensor) -> torch.tensor:
         """
-        Encode input to latent space.
+        Encodes input data and returns latent space data.
+
+        Arguments:
+            x: Input data of shape [input_dim, dim_x+optionalDim].
+
+        Returns:
+            Latent space data [input_dim, dim_z+optionalDim].
         """
         for layer in self.encoderLayers:
             x = layer(x)
         return x
 
-    def decoder(self, x):
+    def decoder(self, z) -> torch.tensor:
         """
-        Decode latent space and reconstruct input x
+        Decodes llatent space and estimates x_hat.
+
+        Arguments:
+            z: Input data of shape [input_dim, dim_z+optionalDim].
+
+        Returns:
+            Estimate of x_hat [input_dim, dim_x+optionalDim ]
         """
         for layer in self.decoderLayers:
-            x = layer(x)
-        return x
+            z = layer(z)
+        return z
 
-    def loss_auto(self, x, x_hat, z_hat):
+    def loss_auto(self, x: torch.tensor, x_hat: torch.tensor, z_hat: torch.tensor) -> torch.tensor:
         """
-        Loss function for autonomous experiment
+        Loss function for autonomous experiment.
+
+        Arguments:
+            x: Input data of shape [input_dim, dim_x+optionalDim].
+            x_hat: Estimated data of shape [input_dim, dim_x+optionalDim].
+            z_hat: Estimated data of shape [input_dim, dim_z+optionalDim].
+
+        Returns:
+            loss: loss1 + loss2
+            loss_1: MSE(x,x_hat)
+            loss_2: MSE(dTdx*f(x),D*z+F*h(x))
         """
+        # Init mean squared error
         mse = nn.MSELoss()
+
+        # Reconstruction loss MSE(x,x_hat)
+        loss_1 = self.params['recon_lambda'] * mse(x, x_hat)
 
         # Compute gradients of T_u with respect to inputs
         dTdx = torch.autograd.functional.jacobian(
             self.encoder, x, create_graph=False, strict=False, vectorize=False)
         dTdx = dTdx[dTdx != 0].reshape((self.params['batch_size'], self.observer.dim_z, self.observer.dim_x))
 
-        loss1 = self.params['recon_lambda'] * mse(x, x_hat)
-
+        # lhs = dTdx * f(x)
         lhs = torch.zeros((self.observer.dim_z, self.params['batch_size']))
         for i in range(self.params['batch_size']):
             lhs[:, i] = torch.matmul(dTdx[i], self.observer.f(x.T).T[i]).T
 
-        rhs = torch.matmul(self.observer.D.to(self.device),
-                           z_hat.T) + torch.matmul(self.observer.F.to(self.device),
-                                               self.observer.h(x.T).to(self.device))
+        # rhs = D * z + F * h(x)
+        D = self.observer.D.to(self.device)
+        F = self.observer.F.to(self.device)
+        h_x = self.observer.h(x.T).to(self.device)
+        rhs = torch.matmul(D, z_hat.T) + torch.matmul(F, h_x)
 
-        loss2 = mse(lhs.to(self.device), rhs)
+        # PDE loss MSE(lhs, rhs)
+        loss_2 = mse(lhs.to(self.device), rhs)
 
-        loss = loss1 + loss2 
-
-        return loss, loss1, loss2
+        return loss_1 + loss_2, loss_1, loss_2
 
     def loss_noise(self, y, y_pred, latent):
         """
-        Loss function for noise experiment
+        # WIP
+        Loss function for noise experiment.
+
+        Arguments:
+            x: Input data of shape [input_dim, dim_x+optionalDim].
+            x_hat: Estimated data of shape [input_dim, dim_x+optionalDim].
+            z_hat: Estimated data of shape [input_dim, dim_z+optionalDim].
+
+        Returns:
+            loss: loss1 + loss2
+            loss1: MSE(x,x_hat)
+            loss2: MSE(dTdx*f(x),D*z+F*h(x))
         """
         w_c = y[:, 0]
 
@@ -107,7 +176,7 @@ class Autoencoder(nn.Module):
         dTdx = dTdy[:, 1:, 1:]
 
         lhs = torch.zeros((self.observer.dim_z, self.params['batch_size']))
-        rhs = lhs.clone() 
+        rhs = lhs.clone()
 
         for i in range(self.params['batch_size']):
             b, a = signal.bessel(3, w_c[i], 'low', analog=True, norm='phase')
@@ -122,20 +191,27 @@ class Autoencoder(nn.Module):
 
         loss2 = mse(lhs.to(self.device), rhs)
 
-
-        loss = loss1 + loss2 
+        loss = loss1 + loss2
 
         return loss, loss1, loss2
 
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        """
+        Forward function for autoencoder. Basically 
+        z = encoder(x)
+        x_hat = decoder(z)
 
-    def forward(self, x):
+        Arguments:
+            x: Input data of shape [input_dim, dim_x+optionalDim].
+
+        Returns:
+            z: Latent space data of shape [input_dim, dim_z+optionalDim].
+            x_hat: Estimated input data of shape [input_dim, dim_x+optionalDim].
         """
-        Takes a batch of samples, encodes them, and then decodes them.
-        """
-        # Compute latent space
+        # Enocde input space
         z = self.encoder(x)
 
-        # Decode laten space
+        # Decode latent space
         x_hat = self.decoder(z)
 
         return z, x_hat
